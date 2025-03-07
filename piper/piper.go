9e77e0e7bdf073cabf645071639894c128da5857
jacob olsen <jacobspil@hotmail.dk>
2025-03-07 03:07:16 +0100

    tts addet need more work

---
+++ piper/piper.go
@@ -0,0 +1,21 @@
+package piper
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func MakeAudio(text string, output string, voice string, playspeed float64) {
+	cmd := exec.Command("piper", "--model", voice, "--length_scale", strconv.FormatFloat(playspeed, 'f', 2, 64), "-f", output)
+	cmd.Stdin = strings.NewReader(text)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func Test() {
+	fmt.Println("done")
+}
